7f6e0a600e173aa557859aa38f72a7e07a76f1e0
maro911220 <kingdomboze@gmail.com>
2024-02-19 23:39:14 +0900

    헤더 검색 영역 추가 (기능 미구현)

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -6,13 +6,15 @@ import { useStore } from "zustand";
 import { defaultStore } from "@/store/store";
 import isLeapYear from "dayjs/plugin/isLeapYear";
 import { BsGearFill, BsFillLightbulbFill } from "react-icons/bs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 dayjs.extend(isLeapYear); // use plugin
 dayjs.locale("ko"); // use locale
 
 export default function Header() {
   const { setEdit, setTheme, checkTheme } = useStore(defaultStore);
+  const [searchData, setSearchData] = useState("");
   const editMode = () => {
     setEdit(true);
   };
@@ -25,6 +27,20 @@ export default function Header() {
     checkTheme();
   }, []);
 
+  const search = () => {
+    if (searchData != "") {
+      axios(`https://dapi.kakao.com/v2/search/web?query=${searchData}`, {
+        headers: {
+          Authorization: "KakaoAK ddb4493be86f80bb366093fa2a1b3e3d",
+        },
+      }).then((res) => {
+        console.log(res);
+      });
+    } else {
+      alert("검색어를 입력해주세요!");
+    }
+  };
+
   return (
     <header className="header">
       <div className="header-con">
@@ -32,6 +48,21 @@ export default function Header() {
         <p className="header-date">
           {dayjs(new Date()).format("YYYY-MM-DD (ddd)")}
         </p>
+        <div className="header-search">
+          <input
+            type="text"
+            className="header-search-input"
+            placeholder="검색어를 입력해주세요"
+            defaultValue={searchData}
+            onChange={(e) => {
+              setSearchData(e.target.value);
+              console.log(searchData);
+            }}
+          />
+          <button className="header-search-btn" onClick={() => search()}>
+            클릭
+          </button>
+        </div>
         <div className="header-btn">
           <button onClick={() => editMode()}>
             <BsGearFill />
