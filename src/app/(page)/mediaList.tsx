ef150d8ebda9fbde6c4f411282cb2968f6c0c6db
maro911220 <kingdomboze@gmail.com>
2024-03-07 23:10:12 +0900

    movie detail update

--- src/app/(page)/mediaList.tsx
+++ src/app/(page)/mediaList.tsx
@@ -1,25 +1,56 @@
 /* eslint-disable @next/next/no-img-element */
-import "./list.scss";
+import "./mediaList.scss";
 
 export default function MediaList({ mediaData }: { mediaData: any }) {
+  console.log(mediaData.credits);
   return (
-    <div>
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${mediaData.poster}`}
-        alt={mediaData.title}
-      />
-      <p>{mediaData.title}</p>
-      <p>{mediaData.tagline}</p>
-      <p>{mediaData.runtime}분</p>
-      <div>
-        {mediaData.genres.map(
-          (item: { id: number; name: string }, index: number) => {
-            return <p key={index}>{item.name}</p>;
-          }
-        )}
+    <div className="media-detail-item">
+      <div className="media-detail-con">
+        <div className="media-detail-img">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${mediaData.poster}`}
+            alt={mediaData.title}
+          />
+        </div>
+
+        <div className="media-detail-text">
+          <p className="media-detail-text__title">{mediaData.title}</p>
+          <p className="media-detail-text__tagline">{mediaData.tagline}</p>
+          <div className="media-detail-box">
+            <div className="media-detail-text__genres">
+              {mediaData.genres.map(
+                (item: { id: number; name: string }, index: number) => {
+                  return <p key={index}>{item.name}</p>;
+                }
+              )}
+            </div>
+            <p className="media-detail-text__runtime">
+              러닝 타임 {mediaData.runtime}분
+            </p>
+            <p className="media-detail-text__release">
+              개봉일 {mediaData.release_date}
+            </p>
+          </div>
+
+          <div className="media-detail-box">
+            <p className="media-detail-text__overview">소개</p>
+            <p>{mediaData.overview}</p>
+          </div>
+        </div>
       </div>
-      <p>{mediaData.release_date}</p>
-      <p>{mediaData.overview}</p>
+      <div className="media-detail-credits">
+        {mediaData.credits?.map((item: any, index: number) => {
+          return (
+            <div key={index}>
+              <img
+                src={`https://media.themoviedb.org/t/p/w138_and_h175_face/${item.profile_path}.jpg`}
+                alt={item.name}
+              />
+              <p>{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/app/(page)/movie/[id]/page.tsx
+++ src/app/(page)/movie/[id]/page.tsx
@@ -1,36 +1,45 @@
 "use client";
 import axios from "axios";
 import MediaList from "@/app/(page)/mediaList";
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 
 export default function Page(props: any) {
   const API_KEY = process.env.TMDB;
   const title = decodeURI(props.params.id);
-  const url = `https://api.themoviedb.org/3/movie/${title}`;
+
   // 데이터를 가져오는 React Query 훅
-  const { isPending, error, data } = useQuery({
-    queryKey: [title],
-    queryFn: () =>
-      axios
-        .get(url, {
+  const ids = [
+    `https://api.themoviedb.org/3/movie/${title}`,
+    `https://api.themoviedb.org/3/movie/${title}/credits`,
+  ];
+
+  const results = useQueries({
+    queries: ids.map((id) => ({
+      queryKey: [title, id],
+      queryFn: () =>
+        axios.get(id, {
           params: { api_key: API_KEY, language: "ko" },
-        })
-        .then((res) => res.data),
+        }),
+      staleTime: Infinity,
+    })),
   });
 
+  const data = results[0].data?.data;
+  const creditsData = results[1].data?.data;
   // Loading & Error
-  if (isPending) return "Loading...";
-  if (error) return "An error has occurred: " + error.message;
-  console.log(data);
+  if (results[0].isPending) return "Loading...";
+  if (results[0].error) return "An error has occurred: ";
+  console.log(creditsData);
 
   const mediaData = {
-    title: data.title,
-    tagline: data.tagline,
-    overview: data.overview,
-    poster: data.poster_path,
-    genres: data.genres,
-    runtime: data.runtime,
-    release_date: data.release_date,
+    title: data?.title,
+    tagline: data?.tagline,
+    overview: data?.overview,
+    poster: data?.poster_path,
+    genres: data?.genres,
+    runtime: data?.runtime,
+    release_date: data?.release_date,
+    credits: creditsData?.cast.slice(0, 10),
   };
 
   return (
