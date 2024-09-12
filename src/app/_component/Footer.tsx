bda18247ac83231112e6aab0527eb17261641487
maro911220 <kingdomboze@gmail.com>
2024-09-12 20:27:38 +0900

    package update, github link update

--- src/app/_component/Footer.tsx
+++ src/app/_component/Footer.tsx
@@ -3,7 +3,7 @@ import "@/styles/component/footer.scss";
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="https://github.com/maro911220" target="_blank">
+      <a href="https://github.com/maro911220/my-board" target="_blank">
         github.com/maro911220
       </a>
     </footer>
